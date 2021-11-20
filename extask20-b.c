a41104d2f1c252cf82161177d98f523011c37657
Viktoriiiii <viktoriasamsonova93@mail.ru>
2021-11-20 20:26:36 +0000

    added extask20-b

---
+++ extask20-b.c
@@ -0,0 +1,35 @@
+#include <stdio.h> // FILE, fopen(), fclose(), fgets()
+#include <string.h> // strlen(), strcpy()
+
+#define max_txt 256
+
+int main()
+{
+    FILE *f = fopen("extask20-a-in.txt", "r");
+    
+    int m = 0;
+    char p[max_txt];    
+    
+    while (1)
+    {
+        char s[max_txt];
+        if (fgets(s, max_txt, f) == NULL) break;        
+        
+        char *c = strchr(s, '\n');
+        if (c != NULL) *c = '\0';
+        
+        int l = strlen(s);
+        if (l > m)
+        {
+            m = l;
+            strcpy(p, s);
+        }
+        printf("%s %d\n", s, l);
+    }
+    
+    printf("str_max = %s, len = %d\n", p, m);
+
+    fclose(f);
+
+    return 0;
+}
