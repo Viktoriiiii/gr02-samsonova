4d8c03fd73fe098dbc3b5e4a90eb951eb97bd287
Viktoriiiii <viktoriasamsonova93@mail.ru>
2021-11-23 18:34:27 +0000

    added extask11-a

---
+++ extask11-a.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    #define rows 25
+    #define cols 5
+    
+    int tab[rows][cols];
+    
+    #define mark0 2
+    #define mark1 5
+    
+    for (int y = 0; y < rows; y++)
+        for (int x = 0; x < cols; x++)
+            tab[y][x] = mark0 + rand() % (mark1 - mark0 + 1);
+    
+    float avg[cols];
+    
+    for (int x = 0; x < cols; x++)
+    {
+        int s = 0;
+        for (int y = 0; y < rows; y++)
+            s += tab[y][x];
+        avg[x] = s / (float) rows;
+    }
+    
+    printf("\033[1;31mnum");
+    for (int x = 0; x < cols; x++)
+        printf("\t%d", x + 1);
+    printf("\n");
+    for (int y = 0; y < rows; y++)
+    {
+        printf("\033[1;35m%d\033[0m", y + 1);
+        for (int x = 0; x < cols; x++)
+            printf("\t%d", tab[y][x]);
+        printf("\n");
+    }
+
+    printf("\033[1;31mavg\033[0m");
+    for (int x = 0; x < cols; x++)
+            printf("\t\033[1;32m%.1f\033[0m", avg[x]);
+    printf("\n");
+    return 0;
+}
